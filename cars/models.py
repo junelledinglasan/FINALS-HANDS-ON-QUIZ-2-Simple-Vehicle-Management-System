56df2d29d996bda3ac1a0e66ef661022d7843d76
junelle dinglasan <junelledinglasan11505@gmail.com>
2026-05-07 10:20:46 +0800

    first commit

---
+++ cars/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Base Model / Parent
+class Vehicle(models.Model):
+    brand = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def vehicle_info(self):
+        return f"{self.brand} costs {self.price}"
+
+    class Meta:
+        abstract = True
+
+# Child Model 1
+class Car(Vehicle):
+    doors = models.IntegerField()
+
+    def vehicle_info(self):
+        return f"{self.brand} Car with {self.doors} doors costs {self.price}"
+
+# Child Model 2
+class Motorcycle(Vehicle):
+    helmet_included = models.BooleanField(default=False)
+
+    def vehicle_info(self):
+        return f"{self.brand} Motorcycle costs {self.price}"
